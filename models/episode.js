3e7a2dd2f9ba7172218757e174f2576cb06f8690
davien21 <chidiebereekennia@gmail.com>
2021-01-27 10:08:19 +0100

    add episode model

---
+++ models/episode.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi)
+
+const mongoose = require("mongoose");
+
+const episodeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 255,
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 1024,
+    },
+    podcastId: mongoose.ObjectId,
+    authorId: mongoose.ObjectId,
+    audioUrl: String
+  },
+  { timestamps: true }
+);
+
+const Episode = mongoose.model("Episode", episodeSchema);
+
+async function validateEpisode(episode) {
+  const schema = {
+    title: Joi.string().min(2).max(255).required(),
+    description: Joi.string().min(2).max(1024).required(),
+    podcastId: Joi.objectId(),
+    authorId: Joi.objectId(),
+    audioUrl: Joi.string().min(2).max(255).required()
+  };
+  const result = await schema.validateAsync(episode);
+
+  return result;
+}
+
+exports.Episode = Episode;
+exports.validate = validateEpisode;
