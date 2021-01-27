7a23301ab2952bcb5ec35e93762af2c6625c1ca2
davien21 <chidiebereekennia@gmail.com>
2021-01-27 10:33:01 +0100

    rename user to users route and use prettier to format file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const userRouter = require('./routes/users')
-const podcastsRouter = require('./routes/podcasts')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const usersRouter = require("./routes/users");
+const podcastsRouter = require("./routes/podcasts");
 // const User = require('./models/user')
 
 // mongoose.connect("mongodb://localhost:27017/pressTest",{ useNewUrlParser: true ,useUnifiedTopology: true })
@@ -15,34 +15,35 @@ const podcastsRouter = require('./routes/podcasts')
 //     return err.message
 // })
 
- mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true ,'useFindAndModify': false, 'useCreateIndex': true, useUnifiedTopology: true})
-    .then(() => 'You are now connected to Mongo!')
-    .catch(err => console.error('Something went wrong', err));
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => "You are now connected to Mongo!")
+  .catch((err) => console.error("Something went wrong", err));
 
 //middlewares
 
-app.use(express.json())
-
-
+app.use(express.json());
 
 //to prevent cors errors
-app.use(cors())
+app.use(cors());
 
+app.get("/", (req, res, next) => {
+  res.send(`Welcome to Press Play API`);
+});
 
-app.get('/', (req,res,next)=>{
-    res.send(`Welcome to Press Play API`)
-})
+app.use("/users", usersRouter);
 
-app.use('/user', userRouter)
+app.use("/api/podcasts", podcastsRouter);
 
-app.use('/api/podcasts', podcastsRouter)
+app.use((req, res, next) => {
+  res.status(404).send(new Error());
+});
 
-app.use((req,res,next)=>{
-    res.status(404).send(new Error)
-})
-
-
-app.listen(process.env.PORT || 4000,()=>{
-    console.log("app listening at port 4000")
-})
+app.listen(process.env.PORT || 4000, () => {
+  console.log("app listening at port 4000");
+});
