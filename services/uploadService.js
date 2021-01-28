e94386dd3e6c27872a41445cd927cc1ed6821fee
davien21 <chidiebereekennia@gmail.com>
2021-01-28 23:20:56 +0100

    update upload service

--- services/uploadService.js
+++ services/uploadService.js
@@ -1,19 +1,8 @@
-const multer = require("multer")
-const shortid = require("shortid")
+const multer = require("multer");
+const shortid = require("shortid");
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    console.log()
-    cb(null, 'uploads')
-  },
-  
-  filename: function (req, file, cb) {
-    const fileExt = file.originalname.split(".").pop();
-    let filename = `${shortid.generate()}_${Date.now()}.${fileExt}`;
-    cb(null, filename);
-  }
-})
- 
-const upload = multer({ storage })
+var storage = multer.diskStorage({ destination: "uploads" });
 
-module.exports = upload
+const upload = multer({ storage });
+
+module.exports = upload;
