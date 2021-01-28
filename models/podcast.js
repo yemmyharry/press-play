31e5f4b6763002181da45fbc9677405097200be5
davien21 <chidiebereekennia@gmail.com>
2021-01-28 14:00:59 +0100

    update podcast model to use userId instead of authorId and add lookup static method

--- models/podcast.js
+++ models/podcast.js
@@ -1,5 +1,4 @@
 const Joi = require("joi");
-Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require("mongoose");
 
 const podcastSchema = new mongoose.Schema(
@@ -16,27 +15,31 @@ const podcastSchema = new mongoose.Schema(
       minlength: 2,
       maxlength: 1024,
     },
-    coverImgUrl: {
+    coverImageUrl: {
       type: String,
       required: true,
       minlength: 2,
       maxlength: 255,
     },
-    authorId: mongoose.ObjectId,
+    userId: mongoose.ObjectId,
   },
   { timestamps: true }
 );
 
-const Podcast = mongoose.model("Episode", podcastSchema);
+const Podcast = mongoose.model("Podcast", podcastSchema);
 
-async function validatePodcast(podcast) {
-  const schema = {
+podcastSchema.statics.lookup = function (title, userId) {
+  return this.findOne({ "title": title, "userId": userId });
+};
+ 
+function validatePodcast(podcast) {
+  const schema = Joi.object({
     title: Joi.string().min(2).max(255).required(),
     description: Joi.string().min(2).max(1024).required(),
-    authorId: Joi.objectId(),
-    coverImgUrl: Joi.string().min(2).max(255).required(),
-  };
-  const result = await schema.validateAsync(podcast);
+    userId: Joi.objectId().required(),
+    coverImageUrl: Joi.string().min(2).max(255).required(),
+  });
+  const result = schema.validate(podcast);
 
   return result;
 }
