67b7a5ab31bda1c7689ee174bf56da4912b009a9
davien21 <chidiebereekennia@gmail.com>
2021-01-29 06:58:49 +0100

    add episodes route and clean up app

--- app.js
+++ app.js
@@ -3,40 +3,41 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("./config/joiObjectId")()
-const multer = require("multer")
-const upload = multer();
 
-const error = require("./middlewares/error")
-
-require("express-async-errors");
-require("./config/logging")()
+const error = require("./middlewares/error");
+require("./config/joiObjectId")();
+require("./config/logging")();
 
 /* Routes */
 const usersRouter = require("./routes/users");
 const podcastsRouter = require("./routes/podcasts");
-
+const episodesRouter = require("./routes/episodes");
 
 /* Database connections */
-// mongoose.connect("mongodb://localhost:27017/pressPlay",{ useNewUrlParser: true ,useUnifiedTopology: true })
-// .then(()=>{
-//     console.log('Connected to mongodb')
-// })
-// .catch((err)=> {
-//     return err.message
-// })
-
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect("mongodb://localhost:27017/pressPlay", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
-    useUnifiedTopology: true,
   })
-  .then(() => "You are now connected to Mongo!")
-  .catch((err) => console.error("Something went wrong", err));
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    return err.message;
+  });
 
-  
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => "You are now connected to Mongo!")
+//   .catch((err) => console.error("Something went wrong", err));
+
 //middlewares
 app.use(express.json());
 
@@ -51,8 +52,9 @@ app.use("/api/users", usersRouter);
 
 app.use("/api/podcasts", podcastsRouter);
 
+app.use("/api/episodes", episodesRouter);
+
 app.use(error);
-  
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("app listening at port 4000");
