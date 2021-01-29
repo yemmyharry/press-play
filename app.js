3c32165ba18ff6fc11a195dd4563db4907a5e683
davien21 <chidiebereekennia@gmail.com>
2021-01-29 09:38:27 +0100

    change mongoose uri to clone version

--- app.js
+++ app.js
@@ -14,29 +14,29 @@ const podcastsRouter = require("./routes/podcasts");
 const episodesRouter = require("./routes/episodes");
 
 /* Database connections */
-mongoose
-  .connect("mongodb://localhost:27017/pressPlay", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("Connected to mongodb");
-  })
-  .catch((err) => {
-    return err.message;
-  });
-
 // mongoose
-//   .connect(process.env.MONGO_URI, {
+//   .connect("mongodb://localhost:27017/pressPlay", {
 //     useNewUrlParser: true,
+//     useUnifiedTopology: true,
 //     useFindAndModify: false,
 //     useCreateIndex: true,
-//     useUnifiedTopology: true,
 //   })
-//   .then(() => "You are now connected to Mongo!")
-//   .catch((err) => console.error("Something went wrong", err));
+//   .then(() => {
+//     console.log("Connected to mongodb");
+//   })
+//   .catch((err) => {
+//     return err.message;
+//   });
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => "You are now connected to Mongo!")
+  .catch((err) => console.error("Something went wrong", err));
 
 //middlewares
 app.use(express.json());
