29bf280811291310dee4d2c2e0ed4f93c821832c
davien21 <chidiebereekennia@gmail.com>
2021-01-29 09:21:58 +0100

    update validateIfExisting middleware

--- middlewares/validateIfExisting.js
+++ middlewares/validateIfExisting.js
@@ -1,5 +1,6 @@
 module.exports = (isExisting, label) => {
   return async (req, res, next) => {
+    if (req.method === "PUT") next()
     const dataAlreadyInDB = await isExisting(req);
     if (dataAlreadyInDB) {
       return res.status(400).send({
