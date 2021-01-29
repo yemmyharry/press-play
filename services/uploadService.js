9d17883cd25a690691c4a4cb109e37397fef5bf1
davien21 <chidiebereekennia@gmail.com>
2021-01-29 03:44:30 +0100

    clean up

--- services/uploadService.js
+++ services/uploadService.js
@@ -1,7 +1,6 @@
 const multer = require("multer");
-const shortid = require("shortid");
 
-var storage = multer.diskStorage({ destination: "uploads" });
+let storage = multer.diskStorage({ dest: "uploads" });
 
 const upload = multer({ storage });
 
