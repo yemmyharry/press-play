0b6c2c3bb1c3c8b4a68fc263fa67605d9fdac1ca
davien21 <chidiebereekennia@gmail.com>
2021-01-29 09:19:48 +0100

    add podcast controller

---
+++ controllers/podcast.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const _ = require("lodash");
+
+const router = express.Router();
+const { Podcast } = require("../models/podcast");
+const { Episode } = require("../models/episode");
+
+exports.createPodcast = async (req, res) => {
+  let podcast = new Podcast(req.body);
+  podcast = await podcast.save();
+  res.send({ status: true, message: null, data: podcast });
+};
+
+exports.updatePodcast = async (req, res) => {
+  let podcast = await Podcast.findByIdAndUpdate(
+    req.params.id,
+    _.pick(req.body, ["title", "description", "coverImageUrl", "userId"]),
+    { new: true }
+  );
+
+  if (!podcast)
+    return res
+      .status(404)
+      .send({ status: false, message: "Invalid Podcast", data: null });
+  res.send({ status: true, message: null, data: podcast });
+};
+
+exports.getAllPodcasts = async (req, res) => {
+  let podcasts = await Podcast.find();
+  res.send(podcasts);
+};
+
+exports.getEpisodesForPodcast = async (req, res) => {
+  const podcastId = req.params.id;
+  let episodes = await Episode.find({ podcastId });
+  res.send(episodes);
+};
+
+exports.getPodcast = async (req, res) => {
+  let podcast = await Podcast.findById(req.params.id);
+  if (!podcast)
+    return res.status(404).send({
+      status: false,
+      message: "Invalid Podcast",
+      data: null,
+    });
+  res.send(podcast);
+};
+
+exports.deletePodcast = async (req, res) => {
+  const podcast = await Podcast.findByIdAndDelete(req.params.id);
+  if (!podcast)
+    return res.send({ status: false, message: "Invalid Podcast", data: null });
+  res.send({ status: true, message: null, data: podcast });
+};
