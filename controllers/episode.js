45e22541bc2a1ab45f4ee560d6624c6828484c4b
davien21 <chidiebereekennia@gmail.com>
2021-02-04 07:26:30 +0100

    fix update issues on episode

--- controllers/episode.js
+++ controllers/episode.js
@@ -2,25 +2,33 @@ const express = require("express");
 const _ = require("lodash");
 
 const router = express.Router();
-const { validateEpisode, Episode } = require("../models/episode");
+const { Episode } = require("../models/episode");
+const { audioUpload } = require("../utils/cloudinary");
+const { updateObject } = require("../utils/helpers");
 
 exports.createEpisode = async (req, res) => {
+  const upload = await audioUpload(req);
+  if (!upload) return res.send({ status: false, message: "Upload error, try again", data: null })
   let episode = new Episode(req.body);
   episode = await episode.save();
   res.send({ status: true, message: null, data: episode });
+
+  
 };
 
 exports.updateEpisode = async (req, res) => {
-  let episode = await Episode.findByIdAndUpdate(
-    req.params.id,
-    _.pick(req.body, ["title", "description", "episodeAudioUrl", "podcastId"]),
-    { new: true }
-  );
-
-  if (!episode)
+  const episodeInDb = await Episode.findById(req.params.id).lean();
+  if (!episodeInDb)
     return res
       .status(404)
       .send({ status: false, message: "Invalid Episode", data: null });
+
+  let updatedEpisode = updateObject(req.body, episodeInDb);
+
+  let episode = await Episode.findByIdAndUpdate(req.params.id, updatedEpisode, {
+    new: true,
+  });
+
   res.send({ status: true, message: null, data: episode });
 };
 
