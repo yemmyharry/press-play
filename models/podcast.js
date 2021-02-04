d513f296de7fa87447f452340a031b8dbcbb6d15
davien21 <chidiebereekennia@gmail.com>
2021-02-04 02:52:02 +0100

    add author in podcast response

--- models/podcast.js
+++ models/podcast.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
-
+const { User } = require("../models/user");
+const _ = require("lodash");
 const podcastSchema = new mongoose.Schema(
   {
     title: {
@@ -26,22 +27,34 @@ const podcastSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-
 podcastSchema.statics.lookup = function (title, userId) {
-  return this.findOne({ "title": title, "userId": userId });
+  return this.findOne({ title: title, userId: userId });
+};
+
+podcastSchema.statics.getOnePodcast = async function (podcastId) {
+  let podcast = await this.findById(podcastId).lean();
+  let authorId = podcast.authorId || podcast.userId;
+  const author = await User.findById(authorId).select("firstName lastName bio");
+  podcast.author = author;
+
+  return podcast;
 };
 
 const Podcast = mongoose.model("Podcast", podcastSchema);
 
 const podcastExists = async function (req) {
-  const podcast = await Podcast.findOne({ "title": req.body.title, "userId": req.body.userId });
-  return podcast ? true : false
-}
- 
+  const podcast = await Podcast.findOne({
+    title: req.body.title,
+    userId: req.body.userId,
+  });
+  return podcast ? true : false;
+};
+
 function validatePodcast(podcast) {
   const schema = Joi.object({
     title: Joi.string().min(2).max(255).required(),
     description: Joi.string().min(2).max(1024).required(),
+    episodeAudio: Joi.any(),
     userId: Joi.objectId().required(),
   });
   const result = schema.validate(podcast);
@@ -49,8 +62,6 @@ function validatePodcast(podcast) {
   return result;
 }
 
-
-
 exports.Podcast = Podcast;
-exports.podcastExists = podcastExists
+exports.podcastExists = podcastExists;
 exports.validatePodcast = validatePodcast;
