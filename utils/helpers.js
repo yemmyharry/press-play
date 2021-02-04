4ad18c8fbc312eb75b336cce0b5d3c0cc6639dff
davien21 <chidiebereekennia@gmail.com>
2021-02-04 03:52:45 +0100

    add helpers util file

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+let formattedDate = (dateString) => {
+  let date = new Date(dateString);
+  const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
+  const month = new Intl.DateTimeFormat('en', { month: "2-digit" }).format(date)
+  const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
+
+  return `${day}-${month}-${year}`
+}
+
+function updateObject(objectToCompare, document) {
+  console.log(objectToCompare, document)
+}
+
+exports.formattedDate = formattedDate
+exports.updateObject = updateObject
