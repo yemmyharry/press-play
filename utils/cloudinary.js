3c332dada78720d075fbdb626f246397ea19e5e4
davien21 <chidiebereekennia@gmail.com>
2021-02-04 07:34:07 +0100

    refactor cloudinary util file

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,8 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 const {
-  CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_SECRET, 
-  CLOUDINARY_API_KEY, CLOUDINARY_ROOT_FOLDER 
-} = process.env
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_ROOT_FOLDER,
+} = process.env;
 
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
@@ -12,21 +14,46 @@ cloudinary.config({
   api_secret: CLOUDINARY_API_SECRET,
 });
 
-async function coverImageUpload (req) {
+async function requireCoverImage(req) {
+  if (!req.file)
+    return res.status(400).send({
+      status: false,
+      message: "coverImage is Required",
+      data: null,
+    });
+}
+
+async function requireAudio(req) {
+  if (!req.file)
+    return res.status(400).send({
+      status: false,
+      message: "Episode Audio is Required",
+      data: null,
+    });
+}
+async function coverImageUpload(req) {
+  requireCoverImage(req);
+
   const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-    resource_type: "image", folder: "press-play/coverImages", use_filename: true 
+    resource_type: "image",
+    folder: "press-play/coverImages",
+    use_filename: true,
   });
   req.body.coverImageUrl = uploadResult.secure_url;
-
+  return uploadResult;
 }
 
-async function audioUpload (req) {
+async function audioUpload(req) {
+  requireAudio(req);
+
   const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-    resource_type: "video", folder: "press-play/audio", use_filename: true 
+    resource_type: "video",
+    folder: "press-play/audio",
+    use_filename: true,
   });
   req.body.episodeAudioUrl = uploadResult.secure_url;
-
+  return uploadResult;
 }
 
-exports.coverImageUpload = coverImageUpload
-exports.audioUpload = audioUpload
+exports.coverImageUpload = coverImageUpload;
+exports.audioUpload = audioUpload;
