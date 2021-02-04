fe901f344d17f47eb2a6c353489cca77d90a00ae
davien21 <chidiebereekennia@gmail.com>
2021-02-04 07:29:39 +0100

    clean up unneeded code after refactor

--- middlewares/handleAudioUploads.js
+++ middlewares/handleAudioUploads.js
@@ -5,20 +5,19 @@ const { validateEpisode, episodeExists } = require("../models/episode");
 const validateBody = require("./validateBody");
 const validateIfExisting = require("./validateIfExisting");
 
-
 async function uploadEpisodeAudio(req, res, next) {
-  if (!req.file) return res.status(400).send({
-    status: false, message: "episodeAudio is Required", data: null
-  });
-  
+  if (!req.file)
+    return res.status(400).send({
+      status: false,
+      message: "episodeAudio is Required",
+      data: null,
+    });
+
   await audioUpload(req);
 
   next();
 }
 
-module.exports = (field) => {
-  return [
-    upload.single(field), validateBody(validateEpisode), 
-    validateIfExisting(episodeExists, "Episode"), uploadEpisodeAudio
-  ];
+module.exports = (field, middlewares) => {
+  return [ upload.single(field), [...middlewares] ];
 };
