2ba3a869d7d524b25a7d100398dcf89ad48a7820
davien21 <chidiebereekennia@gmail.com>
2021-02-04 07:28:32 +0100

    add util function to help with PUT requests

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,29 @@
 let formattedDate = (dateString) => {
   let date = new Date(dateString);
-  const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
-  const month = new Intl.DateTimeFormat('en', { month: "2-digit" }).format(date)
-  const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
+  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  const month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(
+    date
+  );
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
 
-  return `${day}-${month}-${year}`
+  return `${day}-${month}-${year}`;
+};
+
+function updateObject(objectToCompare, objectToUpdate) {
+  objectToUpdateProps = Object.keys(objectToUpdate);
+  for (let property in objectToCompare) {
+    if (
+      property === "_id" ||
+      property === "createdAt" ||
+      property === "updatedAt"
+    )
+      continue;
+    if (objectToUpdateProps.includes(property))
+      objectToUpdate[property] = objectToCompare[property];
+  }
+
+  return objectToUpdate;
 }
 
-function updateObject(objectToCompare, document) {
-  console.log(objectToCompare, document)
-}
-
-exports.formattedDate = formattedDate
-exports.updateObject = updateObject
+exports.formattedDate = formattedDate;
+exports.updateObject = updateObject;
