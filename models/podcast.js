ed41494f038ecc6e69f302563b3ac4a2725aff96
davien21 <chidiebereekennia@gmail.com>
2021-02-05 19:05:38 +0100

    fix circular includes error by injection

--- models/podcast.js
+++ models/podcast.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const { formattedDate } = require("../utils/helpers");
+
 const { Episode } = require("./episode");
+const { User } = require("./user");
 
 const podcastSchema = new mongoose.Schema(
   {
