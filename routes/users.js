9aa85ea4b5a31a5637e2608d552f4942a3d7364f
davien21 <chidiebereekennia@gmail.com>
2021-02-05 10:24:28 +0100

    add routes to get a user/author's podcastData, subscribe and unsubscribe from a podcast

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,10 @@ const {
   activateAccount,
   getLoggedInUser,
   deleteUser,
-  // updateUser,
+  getAllPodcastData,
+  subscribeToPodcast,
+  unSubscribeFromPodcast,
+  updateUser,
 } = require("../controllers/user");
 
 const checkAuth = require("../middlewares/checkAuth");
@@ -19,7 +22,13 @@ const { validateUser, validateLogin } = require("../models/user");
 
 router.get("/me", checkAuth, getLoggedInUser);
 
+router.get("/:id/podcasts", [checkAuth, validateObjectId], getAllPodcastData);
+
 router.post("/signup", validateBody(validateUser), userSignup);
+
+router.post("/subscribe/:id", [checkAuth, validateObjectId], subscribeToPodcast);
+
+router.post("/unsubscribe/:id", [checkAuth, validateObjectId], unSubscribeFromPodcast);
 
 router.get("/activate-account", activateAccount);
 
@@ -29,7 +38,8 @@ router.put("/forgot-password", forgotPassword);
 
 router.put("/reset-password", resetPassword);
 
-// router.put("/:id", validateObjectId, updateUser);
+router.put("/:id", validateObjectId, updateUser);
 
 router.delete("/:id", validateObjectId, deleteUser);
+
 module.exports = router;
