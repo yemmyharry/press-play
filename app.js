bb013d92f07264a5e1778de820378fcf6210640f
davien21 <chidiebereekennia@gmail.com>
2021-02-06 09:56:07 +0100

    configure dotenv to work across multiple environments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` })
 const express = require("express");
 const app = express();
 const winston = require("winston");
