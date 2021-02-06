e3407b99ed19db2d99809f4d97341665052647ad
davien21 <chidiebereekennia@gmail.com>
2021-02-06 04:39:44 +0100

    refactor app

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ require("./config/logging")();
 require("./config/db")();
 require("./routes/index")(app);
  
-/* Database connections */
-
 app.listen(process.env.PORT || 4000, () => {
   console.log("app listening at port 4000");
 });
