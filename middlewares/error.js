737e21f406a08cfab20af927265806e8e5a733a8
davien21 <chidiebereekennia@gmail.com>
2021-07-20 06:08:21 +0100

    update error response

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,8 @@
 module.exports = (winston) => {
   return function (err, req, res, next) {
     winston.error(err.message, err);
-    res.status(500).send('Something failed.')
-  }
-}
+    res
+      .status(500)
+      .send({ status: false, message: "Something failed.", data: null });
+  };
+};
