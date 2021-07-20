946dac9af03189303e4635765f8b26a730801b9d
davien21 <chidiebereekennia@gmail.com>
2021-07-20 06:08:40 +0100

    cleanups

--- middlewares/handleCoverImageUploads.js
+++ middlewares/handleCoverImageUploads.js
@@ -1,8 +1,5 @@
 const upload = require("../services/uploadService");
 
 module.exports = (field, middlewares) => {
-
-  return [
-    upload.single(field), [...middlewares]
-  ];
+  return [upload.single(field), [...middlewares]];
 };
