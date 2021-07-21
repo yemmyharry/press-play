b3313d130f83784150e0296cc83d5804c7fcc254
davien21 <chidiebereekennia@gmail.com>
2021-07-21 14:33:31 +0100

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,21 @@ module.exports = function (app) {
   app.use(cors());
 
   app.get("/", (req, res, next) => {
-    res.send(`Welcome to Press Play API. Documentation available at <a href="https://documenter.getpostman.com/view/9823092/TW74i51A">https://documenter.getpostman.com/view/9823092/TW74i51A.</a>`);
+    res.send(
+      `Welcome to Press Play API. Documentation available at <a href="https://documenter.getpostman.com/view/9823092/TW74i51A">https://documenter.getpostman.com/view/9823092/TW74i51A.</a>`
+    );
   });
 
   app.use("/api/episodes", episodes); // use the episodes router;
   app.use("/api/podcasts", podcasts); // use the podcasts router;
   app.use("/api/users", users); // use the users router;
 
-
   app.use("*", (req, res) => {
-    res.send({ status: false, message: "This is an invalid route", data: null });
+    res.send({
+      status: false,
+      message: "This is an invalid route",
+      data: null,
+    });
   });
   //error middleware
   app.use(error(winston));
